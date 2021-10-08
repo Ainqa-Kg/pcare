9b6c7157c43c73a28b0d1554bcd72f65c3c82327
SathishRakin <sathish.rakin@ainqa.com>
2021-10-08 12:27:25 +0530

    Auto-Commit at :

--- pcare.sh
+++ pcare.sh
@@ -6,7 +6,6 @@ while true
 
 do
 
-	inotifywait --recursive -qq -e attrib,create,delete,modify_self,move,move_self,close_erite $path
 	cd $path > /dev/null &> /dev/null
 	git add --all > /dev/null &> /dev/null
 	git commit -m "Auto-Commit at : " &> /dev/null
